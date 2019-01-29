af34004dd8da1030d04dac51f8e303adc073bb20
Rassa95 <39536503+Rassa95@users.noreply.github.com>
2019-01-29 19:41:23 +0100

    limpieza

--- src/com/sge/erp/view/clients/ClientsAddClientController.java
+++ src/com/sge/erp/view/clients/ClientsAddClientController.java
@@ -12,12 +12,7 @@ import java.net.URL;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 
-public class ClientsAddClientController implements Initializable {
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-
-    }
+public class ClientsAddClientController {
 
     private ManagerClient mc;
     private ClientsController cc;
