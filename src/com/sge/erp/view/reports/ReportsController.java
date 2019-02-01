df414dfe43f23a3d7d489e31a613724a010a0c67
AndresCanadas <39269500+AndresCanadas@users.noreply.github.com>
2019-02-01 22:29:05 +0100

    Reports por finn

---
+++ src/com/sge/erp/view/reports/ReportsController.java
@@ -0,0 +1,55 @@
+package com.sge.erp.view.reports;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ReportsController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+
+    @FXML
+    private AnchorPane container;
+
+    @FXML
+    void completedprojects(MouseEvent event) {
+
+    }
+
+    @FXML
+    void efficientworkers(MouseEvent event) {
+
+    }
+
+    @FXML
+    void employeeswithoutteam(MouseEvent event) {
+
+    }
+
+    @FXML
+    void moreadvanceprojects(MouseEvent event) {
+
+    }
+
+    @FXML
+    void projectstoend(MouseEvent event) {
+
+    }
+
+    @FXML
+    void projectswithmoreclients(MouseEvent event) {
+
+    }
+
+    @FXML
+    void projectswithmoretasks(MouseEvent event) {
+
+    }
+}
