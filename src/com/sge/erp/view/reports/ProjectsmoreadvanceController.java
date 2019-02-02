80b56355e462067256c5d74fb920ff11e67a83b2
AndresCanadas <39269500+AndresCanadas@users.noreply.github.com>
2019-02-02 02:28:11 +0100

    ups xdd

--- src/com/sge/erp/view/reports/ProjectsmoreadvanceController.java
+++ src/com/sge/erp/view/reports/ProjectsmoreadvanceController.java
@@ -77,7 +77,7 @@ public class ProjectsmoreadvanceController implements Initializable {
 
                 }
 
-                if(tasks.size()>=0.5){
+                if(projectComplete>=0.5){
                     FXMLLoader loader = new FXMLLoader(getClass().getResource("projects_card.fxml"));
                     AnchorPane card = loader.load();
                     ProjectCardController cc = loader.getController();
